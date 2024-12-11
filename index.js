90c6e8008a8901672dfec6815d9cd8a5bc36f541
Mani <manied116@gmail.com>
2024-12-11 10:45:45 +0530

    first letter cap, bracket

--- index.js
+++ index.js
@@ -32,3 +32,45 @@ function removeDuplicate(arr){
 let removeDuplicateData = [1,2,4,6,8,3,1,2]
 
 console.log(removeDuplicate(removeDuplicateData))
+
+// change first letter capital
+function capitalizeFirstLetter(string) {
+    let first = string.charAt(0)
+    console.log(first)
+    
+    return first.toUpperCase() + string.slice(1);
+}
+
+console.log(capitalizeFirstLetter("apple"));
+
+// bracket related
+function isValidBrackets(str) {
+    const stack = [];
+    const bracketMap = {
+        '}': '{',
+        ']': '[',
+        ')': '(',
+    };
+
+    for (let char of str) {
+        // console.log(char)
+        if (['{', '[', '('].includes(char)) {
+            // If it's an opening bracket, push it onto the stack
+            stack.push(char);
+        } else if (['}', ']', ')'].includes(char)) {
+            // If it's a closing bracket, check the top of the stack
+            // console.log(stack)
+            // console.log(bracketMap[char])
+            if (stack.pop() !== bracketMap[char]) {
+                return false; // Invalid if it doesn't match
+            }
+        }
+        // console.log(stack)
+    }
+    // If the stack is empty, all brackets were matched
+    return stack.length === 0;
+}
+
+// Example Usage
+const input = "{[]}";
+console.log(isValidBrackets(input))
